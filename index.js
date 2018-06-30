740f991c042a340868a5532e5ae51214d1d6906e
Hezhiyong <15988134176@163.com>
2018-06-30 18:11:18 +0800

    更新demo

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import task from './task.js'
 
-window.task = task
+function appendList(num, status, text) {
+  let li = document.createElement('li')
+  li.innerHTML = `<span style="display:inline-block;width: 150px;">任务序号：${num} </span>
+                  <span style="display:inline-block;width: 150px;">状态：${status} </span>
+                  <span>结果：${text}</span>`
+  list.append(li)
+}
 
-for (let i = 0; i < 20; i++) {
-  task('https://cnodejs.org/api/v1/topics')
-    .then(res => res.json())
-    .then(reslut => {
-      if (reslut.success)
-        console.log(reslut.data[0].title)
-      else 
-        console.log(reslut.error_msg)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+startTask.onclick = function () {
+  for (let i = 0; i < taskCount.value; i++) {
+    task(taskParams.value + `?num=${i}`)
+      .then(res => res.json())
+      .then(reslut => {
+        if (reslut.success)
+          appendList(i, 'success', reslut.data[0].title)
+        else
+          appendList(i, 'failed', reslut.error_msg)
+      })
+      .catch(err => {
+        appendList(i, 'error', err)
+      })
+  }
 }
